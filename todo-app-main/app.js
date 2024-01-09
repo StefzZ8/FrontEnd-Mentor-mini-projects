d72445d3b9e5a2c33fc12071d02455bb67f3df4d
Stefko1998 <Simjanovski998@gmail.com>
2024-01-09 12:31:33 +0100

    onClick change dark/light theme

--- todo-app-main/app.js
+++ todo-app-main/app.js
@@ -1,5 +1,5 @@
 function toggleIcon() {
-    const icon = document.getElementById('petko');
+    const icon = document.getElementById('icon-img');
 
     if (icon) {
 
@@ -9,12 +9,20 @@ function toggleIcon() {
             icon.src = './images/icon-sun.svg';
         }
         function changeToggle() {
-            const element = document.querySelector('.main-section')
-            element.classList.toggle('bg-image')
+            const element = document.querySelector('.main__section')
+            element.classList.toggle('bg__image-swap')
         }
-        
+
+        function changeBodyColor() {
+            const element2 = document.body
+            element2.classList.toggle('body__part')
+        }
+
+
     } else {
         console.error('Element with ID "toggleIcon" not found.');
     }
     changeToggle()
+    changeBodyColor()
+
 }
